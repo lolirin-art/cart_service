4b71b25d4b5887a54f1477af5cc2958a1310a6ae
lolirin-art <karinafedorova616@gmail.com>
2025-08-14 16:29:51 +0300

    Revert "Начало создания сервиса корзины интернет-магазина"
    This reverts commit e4da815eb64fa32f1bf3c13ca231c7655f011675.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("Welcome to the Cart service for the store");
-    println!("Service can do: \n create\n read\n update\n delete");
+
+    println!("Cart service for shop!");
+
 }
